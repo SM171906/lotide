8c0656da61f148ab654a43c31345629fe2f0a893
Smiti <mishra.smiti06@gmail.com>
2021-09-20 14:41:12 -0400

    updated files with mocha & chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,16 @@
-const tail = require('../tail.js');
-const words = ["Yo Yo", "Lighthouse", "Labs"];
 
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-console.log(words.length, 3); // original array should still have 3 elements!
-//assertEqual(tail(words), ["Lighthouse", "Labs"]);
+const assert = require('chai').assert;
+const tail = require('../tail.js'); 
+
+describe("#tail", () => {
+  it("should return [Lighthouse, Labs] for [Hello, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
+
+
+
+
+
+
+
