52bca6c1963fb0afe33cca0d943298387d0252f9
Smiti <mishra.smiti06@gmail.com>
2021-09-20 14:28:17 -0400

    updated mocha & chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,16 @@
-const head = require('../head.js');
-console.log(head([5, 6, 7]), 5);
-console.log(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-console.log(head([5, 6, 7]), 6);
-console.log(head(["Hello", "Lighthouse", "Labs"]), "Helloo");
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
+
+
+
+// console.log(head([5, 6, 7]), 5);
+// console.log(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// console.log(head([5, 6, 7]), 6);
+// console.log(head(["Hello", "Lighthouse", "Labs"]), "Helloo");
